f5f8015dedaef456f5f47e773822cb06d069519a
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-27 18:22:42 +0530

    Add project files.

---
+++ ConsoleAppDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
